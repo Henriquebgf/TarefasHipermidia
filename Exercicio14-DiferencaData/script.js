9a93386eb25e6080b89bf4da6d6e401b1247e6fd
Luis Henrique <l.henrique.b.g.filho@gmail.com>
2021-07-21 14:30:23 -0300

    correção data

--- Exercicio14-DiferencaData/script.js
+++ Exercicio14-DiferencaData/script.js
@@ -1,26 +1,11 @@
-/*
-var data = new Date();
-var dia = String(data.getDate()).padStart(2, '0');
-var mes = String(data.getMonth() + 1).padStart(2, '0');
-var ano = data.getFullYear();
-var dataAtual = dia + '/' + mes + '/' + ano;
 
-var date1 ="23/06/2021";
-
-var diff = moment(date1, 'DD/MM/YYYY').diff(moment(dataAtual,"DD/MM/YYYY"));
-
-var dias = moment.duration(diff).asDays();
-var meses = moment.duration(diff).asMonths();
-
-console.log(dias+" "+meses);
-*/
 var DateDiff = {
 
     inDays: function(d1, d2) {
         var t2 = d2.getTime();
         var t1 = d1.getTime();
 
-        return parseInt((t2-t1)/(24*3600*1000))-1;
+        return parseInt((t2-t1)/(24*3600*1000));
     },
 
     inWeeks: function(d1, d2) {
